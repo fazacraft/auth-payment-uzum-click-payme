eec063e5143402b67aacc0985ca4931beea8b6dd
Fazliddin <phazliddinphozilov@gmail.com>
2025-04-17 20:08:23 +0500

    fixed some issues

--- payment/admin.py
+++ payment/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from payment.models import PaymeOrder
+from payment.models import PaymeOrder, Transaction
 
 
 # Register your models here.
 @admin.register(PaymeOrder)
 class PaymeOrderAdmin(admin.ModelAdmin):
     list_display = ['user', 'order_id', 'amount', 'is_paid']
+
+
+
+@admin.register(Transaction)
+class PaymeTransactionAdmin(admin.ModelAdmin):
+    list_display = ['transaction_id', 'payme_order', 'amount', 'state', 'performed_at', 'canceled_at','created_at', 'updated_at']
