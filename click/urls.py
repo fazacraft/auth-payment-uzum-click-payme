3f55d4847d3877d2b17c57609e4e4f8a6a77d00a
Fazliddin <phazliddinphozilov@gmail.com>
2025-04-22 16:34:30 +0500

    added click urls

--- click/urls.py
+++ click/urls.py
@@ -4,5 +4,6 @@ from click.views import ClickViewSet
 
 
 urlpatterns = [
-    path('payment/', ClickViewSet.as_view({'post': 'prepare'}), name='click_prepare'),
+    path('payment/click/prepare/', ClickViewSet.as_view({'post': 'prepare'}), name='click_prepare'),
+    path('payment/click/complete', ClickViewSet.as_view({'post': 'complete'}), name='click_complete'),
 ]
