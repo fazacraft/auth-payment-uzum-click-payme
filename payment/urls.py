09f47db87379527e9da3bd019640b3eccc4b3547
Fazliddin <phazliddinphozilov@gmail.com>
2025-04-22 16:35:13 +0500

    changed urls payment/ -> payment/payme/

--- payment/urls.py
+++ payment/urls.py
@@ -4,6 +4,6 @@ from authentication.views import UserViewSet, UserLoginViewSet
 from payment.views import PaymeViewSet
 
 urlpatterns = [
-    path('payment/', PaymeViewSet.as_view({'post': 'post'}), name='payment'),
+    path('payment/payme/', PaymeViewSet.as_view({'post': 'post'}), name='payment'),
 
 ]
