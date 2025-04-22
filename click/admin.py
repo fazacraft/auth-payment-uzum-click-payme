51e726dac3c6f0a90f6704646e2996e241b3f1ea
Fazliddin <phazliddinphozilov@gmail.com>
2025-04-22 16:29:17 +0500

    configured admin panel for ClickTransaction

--- click/admin.py
+++ click/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from click.models import ClickTransaction
+
 # Register your models here.
+@admin.register(ClickTransaction)
+class ClickTransactionAdmin(admin.ModelAdmin):
+    list_display = ('merchant_prepare_id','click_trans_id', 'click_paydoc_id','merchant_trans_id', 'created_at', 'updated_at')
