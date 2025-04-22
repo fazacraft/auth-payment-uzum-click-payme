fef5ba84ccd33c0f81ca2bcefcea890c99b6857d
Fazliddin <phazliddinphozilov@gmail.com>
2025-04-22 16:34:44 +0500

    added click urls

--- config/urls.py
+++ config/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('authentication.urls')),
     path('api/v1/', include('payment.urls')),
+    path('api/v1/', include('click.urls')),
 
     re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
