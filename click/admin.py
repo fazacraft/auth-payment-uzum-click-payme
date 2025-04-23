4605fcf1bd0941e843809ad4ef6e150e270fbb07
Fazliddin <phazliddinphozilov@gmail.com>
2025-04-23 18:51:01 +0500

    added extra info

--- click/admin.py
+++ click/admin.py
@@ -5,4 +5,4 @@ from click.models import ClickTransaction
 # Register your models here.
 @admin.register(ClickTransaction)
 class ClickTransactionAdmin(admin.ModelAdmin):
-    list_display = ('merchant_prepare_id','click_trans_id', 'click_paydoc_id','merchant_trans_id', 'created_at', 'updated_at')
+    list_display = ('merchant_prepare_id','click_trans_id', 'state','click_paydoc_id','merchant_trans_id', 'created_at', 'updated_at')
